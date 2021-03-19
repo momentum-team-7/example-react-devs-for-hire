8f84c8c32a80089fc07301b7794394cd05bb595b
Amy Gori <amydgori@gmail.com>
2021-03-19 17:11:19 -0400

    Move App into module

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+
+export const App = () => {
+  const [developers] = useState(['Tatiana', 'Laura', 'Albany'])
+  return (
+    <main>
+      <h1>Developers for Hire!</h1>
+      {developers.map((dev, idx) => (
+        <Developer name={dev} expertise="front-end developer" key={idx} />
+      ))}
+    </main>
+  )
+}
+
+const Developer = (props) => {
+  const { name, expertise } = props
+  return (
+    <div className="dev">
+      <h2>{name}</h2>
+      <p>{expertise}</p>
+    </div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,26 +1,5 @@
-import React, { useState } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
-
-const App = () => {
-  const [developers] = useState(['Tatiana', 'Laura', 'Albany'])
-  return (
-    <main>
-      <h1>Developers for Hire!</h1>
-      {developers.map((dev) => (
-        <Developer name={dev} expertise="front-end developer" />
-      ))}
-    </main>
-  )
-}
-
-const Developer = (props) => {
-  const { name, expertise } = props
-  return (
-    <div className="dev">
-      <h2>{name}</h2>
-      <p>{expertise}</p>
-    </div>
-  )
-}
+import { App } from './App'
 
 ReactDOM.render(<App />, document.querySelector('#root'))
