7101f5e1b9e545e250c5a914448e3ba62b7e36fb
Amy Gori <amydgori@gmail.com>
2021-03-19 17:44:08 -0400

    Add array of objects in state

--- src/App.js
+++ src/App.js
@@ -2,13 +2,41 @@ import { useState } from 'react'
 import './styles/App.css'
 
 export const App = () => {
-  const [developers] = useState(['Tatiana', 'Laura', 'Albany'])
+  const [developers] = useState([
+    {
+      name: 'Tatiana',
+      expertise: 'front-end developer',
+      availableForHire: true,
+    },
+    {
+      name: 'Albany',
+      expertise: 'front-end developer',
+      availableForHire: false,
+    },
+    {
+      name: 'Laura',
+      expertise: 'front-end developer',
+      availableForHire: true,
+    },
+    {
+      name: 'Pearl',
+      expertise: 'back-end developer',
+      availableForHire: false,
+    },
+  ])
+
   return (
     <main>
       <h1>Developers for Hire!</h1>
-      {developers.map((dev, idx) => (
-        <Developer name={dev} expertise="front-end developer" key={idx} />
-      ))}
+      {developers.map((dev, idx) => {
+        if (dev.availableForHire) {
+          return (
+            <Developer name={dev.name} expertise={dev.expertise} key={idx} />
+          )
+        } else {
+          return null
+        }
+      })}
     </main>
   )
 }
